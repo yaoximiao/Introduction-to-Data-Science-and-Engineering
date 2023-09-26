4a8e4abf7358c3d67d8c13bf422ee358f5a80b31
毛晓一 <139993915+yaoximiao@users.noreply.github.com>
2023-09-26 23:07:46 +0800

    Delete 2023_9_26 directory

--- 2023_9_26/2.py
+++
@@ -1,4 +0,0 @@
-# 产生10-20的随机浮点数
-import random
-result = 10 + 10 * random.random()
-print(result)
--- 2023_9_26/3.py
+++
@@ -1,9 +0,0 @@
-import re
-
-my_license = input('请输入身份证:')
-pattern = r"^(^\d{15}$)|(^\d{17}([0-9]|X)$)"
-match = re.match(pattern, my_license)
-if match:
-    print('success')
-else:
-    print('fail')
