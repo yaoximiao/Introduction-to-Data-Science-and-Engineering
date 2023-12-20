0ab4abfa4c2c8a28cdf70cecc7b02d15f63e17b5
毛晓一 <139993915+yaoximiao@users.noreply.github.com>
2023-12-20 14:26:04 +0800

    Delete 2023_10_31 directory

--- 2023_10_31/2023_10_31/seaborn/线图.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-sns.set_style('darkgrid')
-fmri = sns.load_dataset('fmri')
-sns.relplot(x='timepoint', y='signal', hue='event', style='event', col='region', kind='line', data=fmri)
-plt.show()
